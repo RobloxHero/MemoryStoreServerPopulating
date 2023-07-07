34467181c78d99ee092a35cf41dbb5d3465da173
RobloxHero <th1624870@gmail.com>
2023-07-07 13:06:16 -0500

    Setting up actions

---
+++ .github/actions/GenerateImages/src/index.js
@@ -0,0 +1,6 @@
+const { setFailed, getInput, debug } = require( '@actions/core' );
+const { context, getOctokit } = require( '@actions/github' );
+
+( async function main() {
+	debug( 'Hello!' );
+} )();
