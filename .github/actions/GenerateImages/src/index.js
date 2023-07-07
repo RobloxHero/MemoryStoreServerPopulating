152f04dab41de7cd381cbeedf017724ca825374a
RobloxHero <th1624870@gmail.com>
2023-07-07 17:17:11 -0500

    Setting up actions

--- .github/actions/GenerateImages/src/index.js
+++ .github/actions/GenerateImages/src/index.js
@@ -8,13 +8,13 @@ import { JSDOM } from "jsdom"
 function createSvgDocument() {
   const dom = new JSDOM(`<!DOCTYPE html><body></body>`);
   let body = d3.select(dom.window.document.querySelector("body"))
-  body.append("rect")
+  body.append("svg")
       .attr("width", "500px")
       .attr("height", "300px")
       .attr("preserveAspectRatio", true)
       .attr('xmlns', 'http://www.w3.org/2000/svg')
       .attr("backgroundColor", "blue")
-      sharp(body.html())
+      sharp(body)
       .png()
       .toFile("image1.png")
       .then(function(info) {
