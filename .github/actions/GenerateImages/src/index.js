4b209987730251c6cec2c8c367338b6d9a9a9aaf
RobloxHero <th1624870@gmail.com>
2023-07-08 01:20:47 -0500

    Setting up actions

--- .github/actions/GenerateImages/src/index.js
+++ .github/actions/GenerateImages/src/index.js
@@ -14,7 +14,7 @@ let ListItem = `
 
 function createIssueListPng() {
   const canvas = SVG(document.documentElement).size(323, 500)
-  let ListItemGroup = SVG().group().addTo(canvas)
+  let ListItemGroup = SVG().group().link('https://google.com').addTo(canvas)
   let ListItem = SVG('<rect width="323" height="54" rx="9.28" ry="9.28" style="fill: #2c2c3d;"/>').addTo(ListItemGroup)
   let Title = 'Testing the label for the Issues'
   let text = SVG(`<text transform="translate(8.31 20.11) scale(1.14 1)" style="fill: #fff; font-family: AdriannaCondensed-ExtraBold, &apos;Adrianna Condensed&apos;; font-size: 18.78px; font-weight: 700;">${Title}</text>`)
