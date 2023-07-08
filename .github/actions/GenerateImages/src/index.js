39a2c88ec9ba84d6b6f320a93fa9fc18d4505ee8
RobloxHero <th1624870@gmail.com>
2023-07-08 14:37:01 -0500

    Setting up actions

--- .github/actions/GenerateImages/src/index.js
+++ .github/actions/GenerateImages/src/index.js
@@ -15,11 +15,14 @@ function truncate(count, string) {
 }
 
 function createIssueListPng(issues) {
+
   const Canvas = SVG(document.documentElement)
   let CanvasHeight = 0
   let CanvasWidth = 323
+
   let ListItemGroupX = 0
   let ListItemGroupY = 0
+
   let ListItemHeight = 54
   let ListItemWidth = 323 
   let ListItemTextTranslateX = 8.31
@@ -29,6 +32,11 @@ function createIssueListPng(issues) {
   let CornerRadius = 9.28
   let ListItemTopPadding = 5
 
+  let CommentTextTranslateX = 302.56
+  let CommentTextTranslateY = 44.92
+  let CommentTextScaleX = 1.04
+  let CommentTextScaleY = 1
+
   let ListItem = SVG(`<rect width="${ListItemWidth}" height="${ListItemHeight}" rx="${CornerRadius}" ry="${CornerRadius}" style="fill: #2c2c3d;"/>`)
   let ListItemTitle = SVG(`<text style="fill: #fff; font-family: AdriannaCondensed-ExtraBold, &apos;Adrianna Condensed&apos;; font-size: 18.78px; font-weight: 700;"></text>`)
   let CommentIcon = SVG(`<g id="_-Product-Icons" data-name="🔍-Product-Icons">
@@ -36,7 +44,7 @@ function createIssueListPng(issues) {
     <path id="_-Color" data-name="🎨-Color" d="m284.23,45.42c-1.23,0-2.23-.99-2.23-2.22v-5.98c0-1.22,1-2.22,2.23-2.22h9.53c1.23,0,2.23.99,2.23,2.22v5.98c0,1.22-1,2.22-2.23,2.22h-4.54l-3.29,2.56c-.36.28-.88.2-1.15-.17-.11-.15-.16-.33-.16-.51v-1.88h-.39Zm4.56-.87h5.13c.66,0,1.2-.53,1.2-1.19v-5.42c0-.66-.54-1.19-1.2-1.19h-9.84c-.66,0-1.2.53-1.2,1.19v5.42c0,.66.54,1.19,1.2,1.19h1.42v.49s0,1.95,0,1.95l3.29-2.44Z" style="fill: #7788b2;"/>
   </g>
 </g>`)
-  let CommentText = SVG(`<text transform="translate(302.56 44.92) scale(1.04 1)" style="fill: #7788b2; font-family: AdriannaCondensed-ExtraBold, &apos;Adrianna Condensed&apos;; font-size: 12.85px; font-weight: 700;"><tspan x="0" y="0"></tspan></text>`)
+  let CommentText = SVG(`<text style="fill: #7788b2; font-family: AdriannaCondensed-ExtraBold, &apos;Adrianna Condensed&apos;; font-size: 12.85px; font-weight: 700;"></text>`)
   let TodoRect = SVG(`<rect x="9" y="30" width="46" height="18" rx="6.2" ry="6.2" style="fill: #ff7bac;"/>`)
   let TodoText = SVG(`<text transform="translate(15.73 43.63) scale(1.29 1)" style="fill: #2c2c3d; font-family: Roboto-Black, Roboto; font-size: 12.43px; font-weight: 800;"><tspan x="0" y="0" style="letter-spacing: -.01em;">t</tspan><tspan x="4.08" y="0">odo</tspan></text>`)
   let WorkingOnItRect = SVG(`<rect x="9" y="30" width="109" height="18" rx="6.2" ry="6.2" style="fill: #9cbc6f;"/>`)
@@ -71,7 +79,7 @@ function createIssueListPng(issues) {
     // Copy the comment count text and add to list group
     let CommentTextClone = CommentText.clone()
     CommentTextClone.text(issues[i].comments)
-    CommentTextClone.transform({ translateY: ListItemGroupY })
+    CommentTextClone.transform({ translateX: CommentTextTranslateX, translateY: CommentTextTranslateY + ListItemGroupY, scaleX: CommentTextScaleX, scaleY: CommentTextScaleY })
     CommentTextClone.addTo(ListItemGroup).front()
 
     if (issues[i].assignee != null) {
