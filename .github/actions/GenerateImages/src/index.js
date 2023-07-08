6b698429e2b67909f945a334e0c5ce76f88c0a35
RobloxHero <th1624870@gmail.com>
2023-07-08 00:08:35 -0500

    Setting up actions

--- .github/actions/GenerateImages/src/index.js
+++ .github/actions/GenerateImages/src/index.js
@@ -14,12 +14,12 @@ let ListItem = `
 
 function createIssueListPng() {
   const canvas = SVG(document.documentElement)
-  let ListBackground = SVG('<svg xmlns="http://www.w3.org/2000/svg" width="323" height="500" viewBox="0 0 323 54"/>')
+  let ListBackground = SVG('<svg xmlns="http://www.w3.org/2000/svg" width="323" height="500" viewBox="0 0 323 54"/>').linkTo('www.google.com')
   let ListItemGroup = SVG().group().addTo(ListBackground)
   let ListItem = SVG('<rect width="323" height="54" rx="9.28" ry="9.28" style="fill: #2c2c3d;"/>').addTo(ListItemGroup)
   let Title = 'Testing the label for the reviews'
-  var text = SVG(document.createElementNS('http://www.w3.org/2000/svg', 'text')).addTo(ListItemGroup)
-  text.fill('#fff')
+  var text = ListItemGroup.text("hello")
+  console.log(ListBackground.svg())
   fs.writeFileSync('image1.svg', ListBackground.svg())     
 }
 
