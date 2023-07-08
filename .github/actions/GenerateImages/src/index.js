06930795233b1f93cbddb7d82ab9b8869c3cb451
RobloxHero <th1624870@gmail.com>
2023-07-08 14:49:08 -0500

    Setting up actions

--- .github/actions/GenerateImages/src/index.js
+++ .github/actions/GenerateImages/src/index.js
@@ -41,7 +41,7 @@ function createIssueListPng(issues) {
   let TodoTextTranslateY = 43.63
   let TodoTextScaleX = 1.29
   let TodoTextScaleY = 1
-  
+
   let WorkingOnItTextTranslateX = 15.73
   let WorkingOnItTextTranslateY = 43.63
   let WorkingOnItTextScaleX = 1.29
@@ -89,7 +89,7 @@ function createIssueListPng(issues) {
     // Copy the comment count text and add to list group
     let CommentTextClone = CommentText.clone()
     CommentTextClone.text(issues[i].comments)
-    CommentTextClone.transform({ translateX: CommentTextTranslateX, translateY: CommentTextTranslateY + ListItemGroupY, scaleX: CommentTextScaleX, scaleY: CommentTextScaleY })
+    CommentTextClone.transform()
     CommentTextClone.addTo(ListItemGroup).front()
 
     if (issues[i].assignee != null) {
