70bf494986cf84ed869c99cdcccbdc562beed166
RobloxHero <th1624870@gmail.com>
2023-07-07 19:20:46 -0500

    Setting up actions

--- .github/actions/GenerateImages/src/index.js
+++ .github/actions/GenerateImages/src/index.js
@@ -4,26 +4,20 @@ import * as d3 from "d3";
 const { Resvg } = require('@resvg/resvg-js')
 import fs from "fs"
 import { JSDOM } from "jsdom"
-
+//
 let ListItem = `
+<?xml version="1.0" encoding="UTF-8"?>
     <svg id="Background" xmlns="http://www.w3.org/2000/svg" width="323" height="54" viewBox="0 0 323 54">
-        <rect width="323" height="54" rx="9.28" ry="9.28" style="fill: #2c2c3d;">
-            <text color="#ffffff">test</text>
-        </rect>
+      <g id="Frame">
+        <rect width="323" height="54" rx="9.28" ry="9.28" style="fill: #2c2c3d;"/>
+      </g>
+    <text transform="translate(8.31 26.44) scale(.97 1)" style="fill: #fff; font-family: BadaBoomProBB, &apos;BadaBoom Pro BB&apos;; font-size: 26.23px;"><tspan x="0" y="0">Title</tspan></text>
   </svg>
   `
 
 function createIssueListPng() {
-  const opts = {
-    fitTo: {
-      mode: 'width',
-      value: 500,
-    },
-  }
-  const resvg = new Resvg(ListItem)
-  const pngData = resvg.render()
-  const pngBuffer = pngData.asPng()
-  fs.writeFileSync('image1.png', pngBuffer)     
+
+  fs.writeFileSync('image1.svg', pngBuffer)     
 }
 
 ( async function main() {
