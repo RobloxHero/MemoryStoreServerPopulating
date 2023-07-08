7425a52e956f25a12378af890a79021fa7cc3956
RobloxHero <th1624870@gmail.com>
2023-07-08 13:58:42 -0500

    Setting up actions

--- .github/actions/GenerateImages/src/index.js
+++ .github/actions/GenerateImages/src/index.js
@@ -19,16 +19,18 @@ function createIssueListPng(issues) {
   let ListItemGroupX = 0
   let ListItemGroupY = 0
   let ListItemHeight = 54
-  let ListItemWidth = 323
+  let ListItemWidth = 323 
+  let ListItemTextTranslateX = 8.31
+  let ListItemTextTranslateY = 20.11
+  let ListItemTextScaleX = 1.14
+  let ListItemTextScaleY = 1
   let CornerRadius = 9.28
-  let TranslateX = 8.31
-  let TranslateY = 20.11
   let ListItemTopPadding = 5
   let CommentNumber = 12
   let ListItemTitleText = 'Testing the label for the Issues'
 
   let ListItem = SVG(`<rect width="${ListItemWidth}" height="${ListItemHeight}" rx="${CornerRadius}" ry="${CornerRadius}" style="fill: #2c2c3d;"/>`)
-  let ListItemTitle = SVG(`<text scale(1.14 1)" style="fill: #fff; font-family: AdriannaCondensed-ExtraBold, &apos;Adrianna Condensed&apos;; font-size: 18.78px; font-weight: 700;">${ListItemTitleText}</text>`)
+  let ListItemTitle = SVG(`<text style="fill: #fff; font-family: AdriannaCondensed-ExtraBold, &apos;Adrianna Condensed&apos;; font-size: 18.78px; font-weight: 700;">${ListItemTitleText}</text>`)
   let CommentIcon = SVG(`<g id="_-Product-Icons" data-name="🔍-Product-Icons">
   <g id="ic_fluent_comment_24_regular" data-name="ic fluent comment 24 regular">
     <path id="_-Color" data-name="🎨-Color" d="m284.23,45.42c-1.23,0-2.23-.99-2.23-2.22v-5.98c0-1.22,1-2.22,2.23-2.22h9.53c1.23,0,2.23.99,2.23,2.22v5.98c0,1.22-1,2.22-2.23,2.22h-4.54l-3.29,2.56c-.36.28-.88.2-1.15-.17-.11-.15-.16-.33-.16-.51v-1.88h-.39Zm4.56-.87h5.13c.66,0,1.2-.53,1.2-1.19v-5.42c0-.66-.54-1.19-1.2-1.19h-9.84c-.66,0-1.2.53-1.2,1.19v5.42c0,.66.54,1.19,1.2,1.19h1.42v.49s0,1.95,0,1.95l3.29-2.44Z" style="fill: #7788b2;"/>
@@ -57,7 +59,7 @@ function createIssueListPng(issues) {
 
     ListItemTitleText = issues[i].title
     let ListItemTitleClone = ListItemTitle.clone()
-    // ListItemTitleClone.transform({ translateX: TranslateX, translateY: TranslateY + ListItemGroupY })
+    ListItemTitleClone.transform({ translateX: TranslateX, translateY: TranslateY + ListItemGroupY, scaleX: ListItemTextScaleX, scaleY: ListItemTextScaleY })
     ListItemTitleClone.addTo(ListItemGroup).front()
 
     // commentNumber = issues[i].comments
