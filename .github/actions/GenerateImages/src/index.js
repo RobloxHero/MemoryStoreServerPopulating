5ad7449b9af986cdad70b8a057046f8cc64ca561
RobloxHero <th1624870@gmail.com>
2023-07-08 00:23:47 -0500

    Setting up actions

--- .github/actions/GenerateImages/src/index.js
+++ .github/actions/GenerateImages/src/index.js
@@ -14,7 +14,7 @@ let ListItem = `
 
 function createIssueListPng() {
   const canvas = SVG(document.documentElement)
-  let ListBackground = SVG('<svg xmlns="http://www.w3.org/2000/svg" width="323" height="500" viewBox="0 0 323 54"/>').linkTo('www.google.com')
+  let ListBackground = SVG('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="323" height="500" viewBox="0 0 323 54"/>').linkTo('www.google.com')
   let ListItemGroup = SVG().group().addTo(ListBackground)
   let ListItem = SVG('<rect width="323" height="54" rx="9.28" ry="9.28" style="fill: #2c2c3d;"/>').addTo(ListItemGroup)
   let Title = 'Testing the label for the reviews'
