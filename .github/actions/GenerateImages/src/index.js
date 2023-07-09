0a6c1b680185c0ae0a4f8c434e899b63f48cd2a9
RobloxHero <th1624870@gmail.com>
2023-07-09 00:31:02 -0500

    Setting up actions

--- .github/actions/GenerateImages/src/index.js
+++ .github/actions/GenerateImages/src/index.js
@@ -156,8 +156,7 @@ let ListItem = SVG(`<g id="MilestoneList">
 <rect id="ListBackground" x="9" y="107" width="229" height="64" rx="8.94" ry="8.94" style="opacity: .28;"/>
 <text id="Title" transform="translate(16.42 15.68) scale(1.14 1)" style="fill: #fff; font-family: AdriannaCondensed-ExtraBold, &apos;Adrianna Condensed&apos;; font-size: 14.28px; font-weight: 700;"></text>
 <g id="ProgressBar">
-  <path id="ProgressBarBack" d="m21.56,51.04h203.88c1.76,0,3.2,1.43,3.2,3.2h0c0,1.89-1.53,3.42-3.42,3.42H21.67c-1.51,0-2.74-1.23-2.74-2.74v-1.23c0-1.46,1.18-2.64,2.64-2.64Z" style="fill: #527f55; opacity: .42;"/>
-  <rect x="10" y="120" width="209" height="6" style="fill: #39b54a;" rx="2" ry="2"/>
+  <rect id="ProgressBarIcon" x="21" y="168" width="209" height="6" style="fill: #39b54a;" rx="2" ry="2"/>
   </g>
 <text id="CompleteLabel" transform="translate(158.36 45.34) scale(.97 1)" style="fill: #fff; font-family: Roboto-Black, Roboto; font-size: 10.92px; font-weight: 800;"></text>
 <text id="IssuesCountLabel" transform="translate(17.79 44.85) scale(.97 1)" style="fill: #fff; font-family: Roboto-Black, Roboto; font-size: 10.92px; font-weight: 800;"></text>
